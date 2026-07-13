215440c6228c73601044d00cce14372bf7799d9b
AliHassan <ali.hassan@orosenergy.eu>
2026-07-13 13:13:47 +0200

    Fix mapping in BatteryImpl, Battery Interface instead of SymmetricEss

--- io.openems.edge.ess.hyperstrong/src/io/openems/edge/ess/hyperstrong/hypercube/bms/HyperCubeBatteryImpl.java
+++ io.openems.edge.ess.hyperstrong/src/io/openems/edge/ess/hyperstrong/hypercube/bms/HyperCubeBatteryImpl.java
@@ -44,7 +44,6 @@ import io.openems.edge.common.modbusslave.ModbusSlave;
 import io.openems.edge.common.startstop.StartStop;
 import io.openems.edge.common.startstop.StartStoppable;
 import io.openems.edge.common.taskmanager.Priority;
-import io.openems.edge.ess.api.SymmetricEss;
 import io.openems.edge.oros.bms.api.BatteryManagementSystem;
 
 @Designate(ocd = Config.class, factory = true)
@@ -130,13 +129,13 @@ public class HyperCubeBatteryImpl extends AbstractOpenemsModbusComponent impleme
 								.bit(0, HyperCubeBattery.ChannelId.POSITIVE_RELAY_STATUS)
 								.bit(1, HyperCubeBattery.ChannelId.NEGATIVE_RELAY_STATUS)
 						),
-						m(SymmetricEss.ChannelId.MAX_CELL_VOLTAGE,
+						m(Battery.ChannelId.MAX_CELL_VOLTAGE,
 								new UnsignedWordElement(10010)),
-						m(SymmetricEss.ChannelId.MIN_CELL_VOLTAGE,
+						m(Battery.ChannelId.MIN_CELL_VOLTAGE,
 								new UnsignedWordElement(10011)),
-						m(SymmetricEss.ChannelId.MAX_CELL_TEMPERATURE,
+						m(Battery.ChannelId.MAX_CELL_TEMPERATURE,
 								new SignedWordElement(10012), SCALE_FACTOR_MINUS_1),
-						m(SymmetricEss.ChannelId.MIN_CELL_TEMPERATURE,
+						m(Battery.ChannelId.MIN_CELL_TEMPERATURE,
 								new SignedWordElement(10013), SCALE_FACTOR_MINUS_1),
 						m(BatteryManagementSystem.ChannelId.MAX_CELL_VOLTAGE_INDEX,
 								new UnsignedWordElement(10014)),
