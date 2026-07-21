2e13b33fb33ca59107a310f24586fad4de39fb13
Adrian Minde <adrian.minde@mail.de>
2026-07-21 12:23:17 +0200

    Add missing total power factor and getter/setters

--- io.openems.edge.meter.acrel.adl400/src/io/openems/edge/meter/acrel/adl400/AcrelAdl400Meter.java
+++ io.openems.edge.meter.acrel.adl400/src/io/openems/edge/meter/acrel/adl400/AcrelAdl400Meter.java
@@ -4,6 +4,10 @@ import io.openems.common.channel.PersistencePriority;
 import io.openems.common.channel.Unit;
 import io.openems.common.types.OpenemsType;
 import io.openems.edge.common.channel.Doc;
+import io.openems.edge.common.channel.FloatReadChannel;
+import io.openems.edge.common.channel.IntegerReadChannel;
+import io.openems.edge.common.channel.LongReadChannel;
+import io.openems.edge.common.channel.value.Value;
 import io.openems.edge.common.component.OpenemsComponent;
 import io.openems.edge.meter.api.ElectricityMeter;
 
@@ -19,6 +23,7 @@ public interface AcrelAdl400Meter extends ElectricityMeter, OpenemsComponent {
 		VOLTAGE_L3_L1(Doc.of(OpenemsType.INTEGER) //
 				.unit(Unit.MILLIVOLT) //
 				.text("Voltage L3-L1")), //
+
 		VOLTAGE_HARMONIC_DISTORTION_L1(Doc.of(OpenemsType.FLOAT) //
 				.unit(Unit.PERCENT) //
 				.text("Voltage Harmonic Distortion L1")), //
@@ -28,6 +33,7 @@ public interface AcrelAdl400Meter extends ElectricityMeter, OpenemsComponent {
 		VOLTAGE_HARMONIC_DISTORTION_L3(Doc.of(OpenemsType.FLOAT) //
 				.unit(Unit.PERCENT) //
 				.text("Voltage Harmonic Distortion L3")), //
+
 		CURRENT_HARMONIC_DISTORTION_L1(Doc.of(OpenemsType.FLOAT) //
 				.unit(Unit.PERCENT) //
 				.text("Current Harmonic Distortion L1")), //
@@ -37,6 +43,10 @@ public interface AcrelAdl400Meter extends ElectricityMeter, OpenemsComponent {
 		CURRENT_HARMONIC_DISTORTION_L3(Doc.of(OpenemsType.FLOAT) //
 				.unit(Unit.PERCENT) //
 				.text("Current Harmonic Distortion L3")), //
+
+		POWER_FACTOR(Doc.of(OpenemsType.FLOAT) //
+				.unit(Unit.NONE) //
+				.text("Power Factor")), //
 		POWER_FACTOR_L1(Doc.of(OpenemsType.FLOAT) //
 				.unit(Unit.NONE) //
 				.text("Power Factor L1")), //
@@ -46,6 +56,7 @@ public interface AcrelAdl400Meter extends ElectricityMeter, OpenemsComponent {
 		POWER_FACTOR_L3(Doc.of(OpenemsType.FLOAT) //
 				.unit(Unit.NONE) //
 				.text("Power Factor L3")), //
+
 		REACTIVE_LAGGING_ENERGY(Doc.of(OpenemsType.LONG) //
 				.unit(Unit.VOLT_AMPERE_REACTIVE_HOURS) //
 				.persistencePriority(PersistencePriority.HIGH) //
@@ -66,4 +77,425 @@ public interface AcrelAdl400Meter extends ElectricityMeter, OpenemsComponent {
 			return this.doc;
 		}
 	}
+
+	/**
+	 * Gets the Channel for {@link ChannelId#VOLTAGE_L1_L2}.
+	 *
+	 * @return the Channel
+	 */
+	public default IntegerReadChannel getVoltageL1L2Channel() {
+		return this.channel(ChannelId.VOLTAGE_L1_L2);
+	}
+
+	/**
+	 * Gets the Voltage L1-L2 in [mV]. See
+	 * {@link ChannelId#VOLTAGE_L1_L2}.
+	 *
+	 * @return the Channel {@link Value}
+	 */
+	public default Value<Integer> getVoltageL1L2() {
+		return this.getVoltageL1L2Channel().value();
+	}
+
+	/**
+	 * Gets the Channel for {@link ChannelId#VOLTAGE_L2_L3}.
+	 *
+	 * @return the Channel
+	 */
+	public default IntegerReadChannel getVoltageL2L3Channel() {
+		return this.channel(ChannelId.VOLTAGE_L2_L3);
+	}
+
+	/**
+	 * Gets the Voltage L2-L3 in [mV]. See
+	 * {@link ChannelId#VOLTAGE_L2_L3}.
+	 *
+	 * @return the Channel {@link Value}
+	 */
+	public default Value<Integer> getVoltageL2L3() {
+		return this.getVoltageL2L3Channel().value();
+	}
+
+	/**
+	 * Gets the Channel for {@link ChannelId#VOLTAGE_L3_L1}.
+	 *
+	 * @return the Channel
+	 */
+	public default IntegerReadChannel getVoltageL3L1Channel() {
+		return this.channel(ChannelId.VOLTAGE_L3_L1);
+	}
+
+	/**
+	 * Gets the Voltage L3-L1 in [mV]. See
+	 * {@link ChannelId#VOLTAGE_L3_L1}.
+	 *
+	 * @return the Channel {@link Value}
+	 */
+	public default Value<Integer> getVoltageL3L1() {
+		return this.getVoltageL3L1Channel().value();
+	}
+
+	/**
+	 * Gets the Channel for {@link ChannelId#VOLTAGE_HARMONIC_DISTORTION_L1}.
+	 *
+	 * @return the Channel
+	 */
+	public default FloatReadChannel getVoltageHarmonicDistortionL1Channel() {
+		return this.channel(ChannelId.VOLTAGE_HARMONIC_DISTORTION_L1);
+	}
+
+	/**
+	 * Gets the Voltage Harmonic Distortion on L1 in [%]. See
+	 * {@link ChannelId#VOLTAGE_HARMONIC_DISTORTION_L1}.
+	 *
+	 * @return the Channel {@link Value}
+	 */
+	public default Value<Float> getVoltageHarmonicDistortionL1() {
+		return this.getVoltageHarmonicDistortionL1Channel().value();
+	}
+
+	/**
+	 * Gets the Channel for {@link ChannelId#VOLTAGE_HARMONIC_DISTORTION_L2}.
+	 *
+	 * @return the Channel
+	 */
+	public default FloatReadChannel getVoltageHarmonicDistortionL2Channel() {
+		return this.channel(ChannelId.VOLTAGE_HARMONIC_DISTORTION_L2);
+	}
+
+	/**
+	 * Gets the Voltage Harmonic Distortion on L2 in [%]. See
+	 * {@link ChannelId#VOLTAGE_HARMONIC_DISTORTION_L2}.
+	 *
+	 * @return the Channel {@link Value}
+	 */
+	public default Value<Float> getVoltageHarmonicDistortionL2() {
+		return this.getVoltageHarmonicDistortionL2Channel().value();
+	}
+
+	/**
+	 * Gets the Channel for {@link ChannelId#VOLTAGE_HARMONIC_DISTORTION_L3}.
+	 *
+	 * @return the Channel
+	 */
+	public default FloatReadChannel getVoltageHarmonicDistortionL3Channel() {
+		return this.channel(ChannelId.VOLTAGE_HARMONIC_DISTORTION_L3);
+	}
+
+	/**
+	 * Gets the Voltage Harmonic Distortion on L3 in [%]. See
+	 * {@link ChannelId#VOLTAGE_HARMONIC_DISTORTION_L3}.
+	 *
+	 * @return the Channel {@link Value}
+	 */
+	public default Value<Float> getVoltageHarmonicDistortionL3() {
+		return this.getVoltageHarmonicDistortionL3Channel().value();
+	}
+
+	/**
+	 * Gets the Channel for {@link ChannelId#CURRENT_HARMONIC_DISTORTION_L1}.
+	 *
+	 * @return the Channel
+	 */
+	public default FloatReadChannel getCurrentHarmonicDistortionL1Channel() {
+		return this.channel(ChannelId.CURRENT_HARMONIC_DISTORTION_L1);
+	}
+
+	/**
+	 * Gets the Current Harmonic Distortion on L1 in [%]. See
+	 * {@link ChannelId#CURRENT_HARMONIC_DISTORTION_L1}.
+	 *
+	 * @return the Channel {@link Value}
+	 */
+	public default Value<Float> getCurrentHarmonicDistortionL1() {
+		return this.getCurrentHarmonicDistortionL1Channel().value();
+	}
+
+	/**
+	 * Gets the Channel for {@link ChannelId#CURRENT_HARMONIC_DISTORTION_L2}.
+	 *
+	 * @return the Channel
+	 */
+	public default FloatReadChannel getCurrentHarmonicDistortionL2Channel() {
+		return this.channel(ChannelId.CURRENT_HARMONIC_DISTORTION_L2);
+	}
+
+	/**
+	 * Gets the Current Harmonic Distortion on L2 in [%]. See
+	 * {@link ChannelId#CURRENT_HARMONIC_DISTORTION_L2}.
+	 *
+	 * @return the Channel {@link Value}
+	 */
+	public default Value<Float> getCurrentHarmonicDistortionL2() {
+		return this.getCurrentHarmonicDistortionL2Channel().value();
+	}
+
+	/**
+	 * Gets the Channel for {@link ChannelId#CURRENT_HARMONIC_DISTORTION_L3}.
+	 *
+	 * @return the Channel
+	 */
+	public default FloatReadChannel getCurrentHarmonicDistortionL3Channel() {
+		return this.channel(ChannelId.CURRENT_HARMONIC_DISTORTION_L3);
+	}
+
+	/**
+	 * Gets the Current Harmonic Distortion on L3 in [%]. See
+	 * {@link ChannelId#CURRENT_HARMONIC_DISTORTION_L3}.
+	 *
+	 * @return the Channel {@link Value}
+	 */
+	public default Value<Float> getCurrentHarmonicDistortionL3() {
+		return this.getCurrentHarmonicDistortionL3Channel().value();
+	}
+
+	/**
+	 * Gets the Channel for {@link ChannelId#POWER_FACTOR}.
+	 *
+	 * @return the Channel for the voltage ratio
+	 */
+	public default FloatReadChannel getPowerFactorChannel() {
+		return this.channel(ChannelId.POWER_FACTOR);
+	}
+
+	/**
+	 * Gets the Power Factor.
+	 *
+	 * @return the Channel {@link Value} containing the power factor
+	 */
+	public default Value<Float> getPowerFactor() {
+		return this.getPowerFactorChannel().value();
+	}
+
+	/**
+	 * Internal method to set the 'nextValue' on {@link ChannelId#POWER_FACTOR}
+	 * Channel.
+	 *
+	 * @param value the next value
+	 */
+	public default void _setPowerFactor(Float value) {
+		this.getPowerFactorChannel().setNextValue(value);
+	}
+
+	/**
+	 * Internal method to set the 'nextValue' on {@link ChannelId#POWER_FACTOR}
+	 * Channel.
+	 *
+	 * @param value the next value
+	 */
+	public default void _setPowerFactor(float value) { this.getPowerFactorChannel().setNextValue(value); }
+
+	/**
+	 * Gets the Channel for {@link ChannelId#POWER_FACTOR_L1}.
+	 *
+	 * @return the Channel for the voltage ratio
+	 */
+	public default FloatReadChannel getPowerFactorL1Channel() {
+		return this.channel(ChannelId.POWER_FACTOR_L1);
+	}
+
+	/**
+	 * Gets the Power Factor of L1.
+	 *
+	 * @return the Channel {@link Value} containing the power factor
+	 */
+	public default Value<Float> getPowerFactorL1() {
+		return this.getPowerFactorL1Channel().value();
+	}
+
+	/**
+	 * Internal method to set the 'nextValue' on {@link ChannelId#POWER_FACTOR_L1}
+	 * Channel.
+	 *
+	 * @param value the next value
+	 */
+	public default void _setPowerFactorL1(Float value) {
+		this.getPowerFactorL1Channel().setNextValue(value);
+	}
+
+	/**
+	 * Internal method to set the 'nextValue' on {@link ChannelId#POWER_FACTOR_L1}
+	 * Channel.
+	 *
+	 * @param value the next value
+	 */
+	public default void _setPowerFactorL1(float value) {
+		this.getPowerFactorL1Channel().setNextValue(value);
+	}
+
+	/**
+	 * Gets the Channel for {@link ChannelId#POWER_FACTOR_L2}.
+	 *
+	 * @return the Channel for the voltage ratio
+	 */
+	public default FloatReadChannel getPowerFactorL2Channel() {
+		return this.channel(ChannelId.POWER_FACTOR_L2);
+	}
+
+	/**
+	 * Gets the Power Factor of L2.
+	 *
+	 * @return the Channel {@link Value} containing the power factor
+	 */
+	public default Value<Float> getPowerFactorL2() {
+		return this.getPowerFactorL2Channel().value();
+	}
+
+	/**
+	 * Internal method to set the 'nextValue' on {@link ChannelId#POWER_FACTOR_L2}
+	 * Channel.
+	 *
+	 * @param value the next value
+	 */
+	public default void _setPowerFactorL2(Float value) {
+		this.getPowerFactorL2Channel().setNextValue(value);
+	}
+
+	/**
+	 * Internal method to set the 'nextValue' on {@link ChannelId#POWER_FACTOR_L2}
+	 * Channel.
+	 *
+	 * @param value the next value
+	 */
+	public default void _setPowerFactorL2(float value) {
+		this.getPowerFactorL2Channel().setNextValue(value);
+	}
+
+	/**
+	 * Gets the Channel for {@link ChannelId#POWER_FACTOR_L3}.
+	 *
+	 * @return the Channel for the voltage ratio
+	 */
+	public default FloatReadChannel getPowerFactorL3Channel() {
+		return this.channel(ChannelId.POWER_FACTOR_L3);
+	}
+
+	/**
+	 * Gets the Power Factor of L3.
+	 *
+	 * @return the Channel {@link Value} containing the power factor
+	 */
+	public default Value<Float> getPowerFactorL3() {
+		return this.getPowerFactorL3Channel().value();
+	}
+
+	/**
+	 * Internal method to set the 'nextValue' on {@link ChannelId#POWER_FACTOR_L3}
+	 * Channel.
+	 *
+	 * @param value the next value
+	 */
+	public default void _setPowerFactorL3(Float value) {
+		this.getPowerFactorL3Channel().setNextValue(value);
+	}
+
+	/**
+	 * Internal method to set the 'nextValue' on {@link ChannelId#POWER_FACTOR_L3}
+	 * Channel.
+	 *
+	 * @param value the next value
+	 */
+	public default void _setPowerFactorL3(float value) {
+		this.getPowerFactorL3Channel().setNextValue(value);
+	}
+
+	/**
+	 * Gets the Channel for {@link ChannelId#REACTIVE_LAGGING_ENERGY}.
+	 *
+	 * @return the Channel
+	 */
+	public default LongReadChannel getReactiveLaggingEnergyChannel() {
+		return this.channel(ChannelId.REACTIVE_LAGGING_ENERGY);
+	}
+
+	/**
+	 * Gets the Reactive Lagging Energy in [varh]. This relates to positive
+	 * (lagging/inductive) REACTIVE_POWER. See
+	 * {@link ChannelId#REACTIVE_LAGGING_ENERGY}.
+	 *
+	 * @return the Channel {@link Value}
+	 */
+	public default Value<Long> getReactiveLaggingEnergy() {
+		return this.getReactiveLaggingEnergyChannel().value();
+	}
+
+	/**
+	 * Internal method to set the 'nextValue' on
+	 * {@link ChannelId#REACTIVE_LAGGING_ENERGY} Channel.
+	 *
+	 * @param value the next value
+	 */
+	public default void _setReactiveLaggingEnergy(Long value) {
+		this.getReactiveLaggingEnergyChannel().setNextValue(value);
+	}
+
+	/**
+	 * Internal method to set the 'nextValue' on
+	 * {@link ChannelId#REACTIVE_LAGGING_ENERGY} Channel.
+	 *
+	 * @param value the next value
+	 */
+	public default void _setReactiveLaggingEnergy(long value) {
+		this.getReactiveLaggingEnergyChannel().setNextValue(value);
+	}
+
+	/**
+	 * Gets the Channel for {@link ChannelId#REACTIVE_LEADING_ENERGY}.
+	 *
+	 * @return the Channel
+	 */
+	public default LongReadChannel getReactiveLeadingEnergyChannel() {
+		return this.channel(ChannelId.REACTIVE_LEADING_ENERGY);
+	}
+
+	/**
+	 * Gets the Reactive Leading Energy in [varh]. This relates to negative
+	 * (leading/capacitive) REACTIVE_POWER. See
+	 * {@link ChannelId#REACTIVE_LEADING_ENERGY}.
+	 *
+	 * @return the Channel {@link Value}
+	 */
+	public default Value<Long> getReactiveLeadingEnergy() {
+		return this.getReactiveLeadingEnergyChannel().value();
+	}
+
+	/**
+	 * Internal method to set the 'nextValue' on
+	 * {@link ChannelId#REACTIVE_LEADING_ENERGY} Channel.
+	 *
+	 * @param value the next value
+	 */
+	public default void _setReactiveLeadingEnergy(Long value) {
+		this.getReactiveLeadingEnergyChannel().setNextValue(value);
+	}
+
+	/**
+	 * Internal method to set the 'nextValue' on
+	 * {@link ChannelId#REACTIVE_LEADING_ENERGY} Channel.
+	 *
+	 * @param value the next value
+	 */
+	public default void _setReactiveLeadingEnergy(long value) {
+		this.getReactiveLeadingEnergyChannel().setNextValue(value);
+	}
+
+	public static void calculatePhaseVoltages(AcrelAdl400Meter meter) {
+		meter.getVoltageL1L2Channel().onSetNextValue(value -> {
+			meter._setVoltageL1(calculatePhaseVoltage(value.get()));
+		});
+		meter.getVoltageL2L3Channel().onSetNextValue(value -> {
+			meter._setVoltageL2(calculatePhaseVoltage(value.get()));
+		});
+		meter.getVoltageL3L1Channel().onSetNextValue(value -> {
+			meter._setVoltageL3(calculatePhaseVoltage(value.get()));
+		});
+	}
+
+	private static Integer calculatePhaseVoltage(Integer phaseToPhaseVoltage) {
+		if (phaseToPhaseVoltage == null) {
+			return null;
+		}
+		return (int) Math.round(phaseToPhaseVoltage / Math.sqrt(3));
+	}
+
 }
--- io.openems.edge.meter.acrel.adl400/src/io/openems/edge/meter/acrel/adl400/AcrelAdl400MeterImpl.java
+++ io.openems.edge.meter.acrel.adl400/src/io/openems/edge/meter/acrel/adl400/AcrelAdl400MeterImpl.java
@@ -2,11 +2,9 @@ package io.openems.edge.meter.acrel.adl400;
 
 import static io.openems.edge.bridge.modbus.api.ElementToChannelConverter.SCALE_FACTOR_2;
 import static io.openems.edge.bridge.modbus.api.ElementToChannelConverter.SCALE_FACTOR_3;
-import static io.openems.edge.bridge.modbus.api.ElementToChannelConverter.SCALE_FACTOR_MINUS_3;
 import static io.openems.edge.bridge.modbus.api.ElementToChannelConverter.SCALE_FACTOR_3_AND_INVERT_IF_TRUE;
+import static io.openems.edge.bridge.modbus.api.ElementToChannelConverter.SCALE_FACTOR_MINUS_3;
 import static io.openems.edge.bridge.modbus.api.element.WordOrder.LSWMSW;
-
-import java.util.function.Consumer;
 
 import org.osgi.service.cm.ConfigurationAdmin;
 import org.osgi.service.component.ComponentContext;
@@ -32,14 +30,11 @@ import io.openems.edge.bridge.modbus.api.element.FloatDoublewordElement;
 import io.openems.edge.bridge.modbus.api.element.UnsignedDoublewordElement;
 import io.openems.edge.bridge.modbus.api.element.UnsignedWordElement;
 import io.openems.edge.bridge.modbus.api.task.FC3ReadRegistersTask;
-import io.openems.edge.common.channel.Channel;
-import io.openems.edge.common.channel.value.Value;
 import io.openems.edge.common.component.OpenemsComponent;
 import io.openems.edge.common.modbusslave.ModbusSlave;
 import io.openems.edge.common.modbusslave.ModbusSlaveNatureTable;
 import io.openems.edge.common.modbusslave.ModbusSlaveTable;
 import io.openems.edge.common.taskmanager.Priority;
-import io.openems.edge.common.type.TypeUtils;
 import io.openems.edge.meter.api.ElectricityMeter;
 
 @Designate(ocd = Config.class, factory = true)
@@ -74,36 +69,19 @@ public class AcrelAdl400MeterImpl extends AbstractOpenemsModbusComponent
 
 	@Activate
 	private void activate(ComponentContext context, Config config) throws OpenemsException {
-		this.type = config.type();
-		this.invert = config.invert();
-		this.phaseWiring = config.phaseWiring();
-		
 		if (super.activate(context, config.id(), config.alias(), config.enabled(), config.modbusUnitId(), this.cm,
 				"Modbus", config.modbus_id())) {
 			return;
 		}
+		this.type = config.type();
+		this.invert = config.invert();
+		this.phaseWiring = config.phaseWiring();
 
-		if (this.phaseWiring == PhaseWiring.THREE_PHASE_FOUR_WIRE) {
-			ElectricityMeter.calculateAverageVoltageFromPhases(this);
-		} else {
-			this.calculateAverageVoltageFromLineToLine();
+		if (this.phaseWiring == PhaseWiring.THREE_PHASE_THREE_WIRE) {
+			AcrelAdl400Meter.calculatePhaseVoltages(this);
 		}
+		ElectricityMeter.calculateAverageVoltageFromPhases(this);
 		ElectricityMeter.calculateSumCurrentFromPhases(this);
-	}
-	
-	private void calculateAverageVoltageFromLineToLine() {
-		final Channel<Integer> l1l2 = this.channel(AcrelAdl400Meter.ChannelId.VOLTAGE_L1_L2);
-		final Channel<Integer> l2l3 = this.channel(AcrelAdl400Meter.ChannelId.VOLTAGE_L2_L3);
-		final Channel<Integer> l3l1 = this.channel(AcrelAdl400Meter.ChannelId.VOLTAGE_L3_L1);
-		final Consumer<Value<Integer>> calculateAverageVoltage = ignore -> {
-			this._setVoltage(TypeUtils.averageRounded(//
-					l1l2.getNextValue().get(), //
-					l2l3.getNextValue().get(), //
-					l3l1.getNextValue().get()));
-		};
-		l1l2.onSetNextValue(calculateAverageVoltage);
-		l2l3.onSetNextValue(calculateAverageVoltage);
-		l3l1.onSetNextValue(calculateAverageVoltage);
 	}
 
 	@Deactivate
@@ -111,101 +89,101 @@ public class AcrelAdl400MeterImpl extends AbstractOpenemsModbusComponent
 		super.deactivate();
 	}
 
-	private static FloatDoublewordElement float32(int address) {
-		return new FloatDoublewordElement(address).wordOrder(LSWMSW);
-	}
-
-	private static UnsignedDoublewordElement uint32(int address) {
-		return new UnsignedDoublewordElement(address).wordOrder(LSWMSW);
+	@Override
+	public MeterType getMeterType() {
+		return this.type;
 	}
 
 	@Override
 	protected ModbusProtocol defineModbusProtocol() {
 		var modbusProtocol = new ModbusProtocol(this, //
-				
 				new FC3ReadRegistersTask(0x084C, Priority.LOW, //
 						m(this.invert ? ElectricityMeter.ChannelId.ACTIVE_CONSUMPTION_ENERGY
 								: ElectricityMeter.ChannelId.ACTIVE_PRODUCTION_ENERGY,
-								uint32(0x084C), SCALE_FACTOR_2),
+								UINT32(0x084C), SCALE_FACTOR_2),
 						new DummyRegisterElement(0x084E, 0x0855),
 						m(this.invert ? ElectricityMeter.ChannelId.ACTIVE_PRODUCTION_ENERGY
 								: ElectricityMeter.ChannelId.ACTIVE_CONSUMPTION_ENERGY,
-								uint32(0x0856), SCALE_FACTOR_2),
+								UINT32(0x0856), SCALE_FACTOR_2),
 						new DummyRegisterElement(0x0858, 0x0869),
 						m(this.invert ? AcrelAdl400Meter.ChannelId.REACTIVE_LEADING_ENERGY
 								: AcrelAdl400Meter.ChannelId.REACTIVE_LAGGING_ENERGY,
-								uint32(0x086A), SCALE_FACTOR_2),
+								UINT32(0x086A), SCALE_FACTOR_2),
 						new DummyRegisterElement(0x086C, 0x0873),
 						m(this.invert ? AcrelAdl400Meter.ChannelId.REACTIVE_LAGGING_ENERGY
 								: AcrelAdl400Meter.ChannelId.REACTIVE_LEADING_ENERGY,
-								uint32(0x0874), SCALE_FACTOR_2)
+								UINT32(0x0874), SCALE_FACTOR_2)
 				)
 		);
 
 		modbusProtocol.addTask(new FC3ReadRegistersTask(0x05DD, Priority.LOW, //
-				m(AcrelAdl400Meter.ChannelId.VOLTAGE_HARMONIC_DISTORTION_L1, new UnsignedWordElement(0x05DD), SCALE_FACTOR_MINUS_3),
-				m(AcrelAdl400Meter.ChannelId.VOLTAGE_HARMONIC_DISTORTION_L2, new UnsignedWordElement(0x05DE), SCALE_FACTOR_MINUS_3),
-				m(AcrelAdl400Meter.ChannelId.VOLTAGE_HARMONIC_DISTORTION_L3, new UnsignedWordElement(0x05DF), SCALE_FACTOR_MINUS_3),
-				m(AcrelAdl400Meter.ChannelId.CURRENT_HARMONIC_DISTORTION_L1, new UnsignedWordElement(0x05E0), SCALE_FACTOR_MINUS_3),
-				m(AcrelAdl400Meter.ChannelId.CURRENT_HARMONIC_DISTORTION_L2, new UnsignedWordElement(0x05E1), SCALE_FACTOR_MINUS_3),
-				m(AcrelAdl400Meter.ChannelId.CURRENT_HARMONIC_DISTORTION_L3, new UnsignedWordElement(0x05E2), SCALE_FACTOR_MINUS_3)
+				m(AcrelAdl400Meter.ChannelId.VOLTAGE_HARMONIC_DISTORTION_L1, new UnsignedWordElement(0x05DD),
+						SCALE_FACTOR_MINUS_3),
+				m(AcrelAdl400Meter.ChannelId.VOLTAGE_HARMONIC_DISTORTION_L2, new UnsignedWordElement(0x05DE),
+						SCALE_FACTOR_MINUS_3),
+				m(AcrelAdl400Meter.ChannelId.VOLTAGE_HARMONIC_DISTORTION_L3, new UnsignedWordElement(0x05DF),
+						SCALE_FACTOR_MINUS_3),
+				m(AcrelAdl400Meter.ChannelId.CURRENT_HARMONIC_DISTORTION_L1, new UnsignedWordElement(0x05E0),
+						SCALE_FACTOR_MINUS_3),
+				m(AcrelAdl400Meter.ChannelId.CURRENT_HARMONIC_DISTORTION_L2, new UnsignedWordElement(0x05E1),
+						SCALE_FACTOR_MINUS_3),
+				m(AcrelAdl400Meter.ChannelId.CURRENT_HARMONIC_DISTORTION_L3, new UnsignedWordElement(0x05E2),
+						SCALE_FACTOR_MINUS_3)
 		));
-	
 		if (this.phaseWiring == PhaseWiring.THREE_PHASE_FOUR_WIRE) {
 			modbusProtocol.addTask(new FC3ReadRegistersTask(0x0800, Priority.HIGH, //
-					m(ElectricityMeter.ChannelId.VOLTAGE_L1, float32(0x0800), SCALE_FACTOR_3),
-					m(ElectricityMeter.ChannelId.VOLTAGE_L2, float32(0x0802), SCALE_FACTOR_3),
-					m(ElectricityMeter.ChannelId.VOLTAGE_L3, float32(0x0804), SCALE_FACTOR_3),
+					m(ElectricityMeter.ChannelId.VOLTAGE_L1, FLOAT32(0x0800), SCALE_FACTOR_3),
+					m(ElectricityMeter.ChannelId.VOLTAGE_L2, FLOAT32(0x0802), SCALE_FACTOR_3),
+					m(ElectricityMeter.ChannelId.VOLTAGE_L3, FLOAT32(0x0804), SCALE_FACTOR_3),
 					new DummyRegisterElement(0x0806, 0x080B),
-					m(ElectricityMeter.ChannelId.CURRENT_L1, float32(0x080C), SCALE_FACTOR_3),
-					m(ElectricityMeter.ChannelId.CURRENT_L2, float32(0x080E), SCALE_FACTOR_3),
-					m(ElectricityMeter.ChannelId.CURRENT_L3, float32(0x0810), SCALE_FACTOR_3),
+					m(ElectricityMeter.ChannelId.CURRENT_L1, FLOAT32(0x080C), SCALE_FACTOR_3),
+					m(ElectricityMeter.ChannelId.CURRENT_L2, FLOAT32(0x080E), SCALE_FACTOR_3),
+					m(ElectricityMeter.ChannelId.CURRENT_L3, FLOAT32(0x0810), SCALE_FACTOR_3),
 					new DummyRegisterElement(0x0812, 0x0813),
-					m(ElectricityMeter.ChannelId.ACTIVE_POWER_L1, float32(0x0814), SCALE_FACTOR_3_AND_INVERT_IF_TRUE(this.invert)),
-					m(ElectricityMeter.ChannelId.ACTIVE_POWER_L2, float32(0x0816), SCALE_FACTOR_3_AND_INVERT_IF_TRUE(this.invert)),
-					m(ElectricityMeter.ChannelId.ACTIVE_POWER_L3, float32(0x0818), SCALE_FACTOR_3_AND_INVERT_IF_TRUE(this.invert)),
-					m(ElectricityMeter.ChannelId.ACTIVE_POWER, float32(0x081A), SCALE_FACTOR_3_AND_INVERT_IF_TRUE(this.invert)),
-					m(ElectricityMeter.ChannelId.REACTIVE_POWER_L1, float32(0x081C), SCALE_FACTOR_3_AND_INVERT_IF_TRUE(this.invert)),
-					m(ElectricityMeter.ChannelId.REACTIVE_POWER_L2, float32(0x081E), SCALE_FACTOR_3_AND_INVERT_IF_TRUE(this.invert)),
-					m(ElectricityMeter.ChannelId.REACTIVE_POWER_L3, float32(0x0820), SCALE_FACTOR_3_AND_INVERT_IF_TRUE(this.invert)),
-					m(ElectricityMeter.ChannelId.REACTIVE_POWER, float32(0x0822), SCALE_FACTOR_3_AND_INVERT_IF_TRUE(this.invert)),
+					m(ElectricityMeter.ChannelId.ACTIVE_POWER_L1, FLOAT32(0x0814),
+							SCALE_FACTOR_3_AND_INVERT_IF_TRUE(this.invert)),
+					m(ElectricityMeter.ChannelId.ACTIVE_POWER_L2, FLOAT32(0x0816),
+							SCALE_FACTOR_3_AND_INVERT_IF_TRUE(this.invert)),
+					m(ElectricityMeter.ChannelId.ACTIVE_POWER_L3, FLOAT32(0x0818),
+							SCALE_FACTOR_3_AND_INVERT_IF_TRUE(this.invert)),
+					m(ElectricityMeter.ChannelId.ACTIVE_POWER, FLOAT32(0x081A),
+							SCALE_FACTOR_3_AND_INVERT_IF_TRUE(this.invert)),
+					m(ElectricityMeter.ChannelId.REACTIVE_POWER_L1, FLOAT32(0x081C),
+							SCALE_FACTOR_3_AND_INVERT_IF_TRUE(this.invert)),
+					m(ElectricityMeter.ChannelId.REACTIVE_POWER_L2, FLOAT32(0x081E),
+							SCALE_FACTOR_3_AND_INVERT_IF_TRUE(this.invert)),
+					m(ElectricityMeter.ChannelId.REACTIVE_POWER_L3, FLOAT32(0x0820),
+							SCALE_FACTOR_3_AND_INVERT_IF_TRUE(this.invert)),
+					m(ElectricityMeter.ChannelId.REACTIVE_POWER, FLOAT32(0x0822),
+							SCALE_FACTOR_3_AND_INVERT_IF_TRUE(this.invert)),
 					new DummyRegisterElement(0x0824, 0x082B),
-					m(AcrelAdl400Meter.ChannelId.POWER_FACTOR_L1, float32(0x082C)),
-					m(AcrelAdl400Meter.ChannelId.POWER_FACTOR_L2, float32(0x082E)),
-					m(AcrelAdl400Meter.ChannelId.POWER_FACTOR_L3, float32(0x0830)),
-					new DummyRegisterElement(0x0832, 0x0833),
-					m(ElectricityMeter.ChannelId.FREQUENCY, float32(0x0834), SCALE_FACTOR_3)
+					m(AcrelAdl400Meter.ChannelId.POWER_FACTOR_L1, FLOAT32(0x082C)),
+					m(AcrelAdl400Meter.ChannelId.POWER_FACTOR_L2, FLOAT32(0x082E)),
+					m(AcrelAdl400Meter.ChannelId.POWER_FACTOR_L3, FLOAT32(0x0830)),
+					m(AcrelAdl400Meter.ChannelId.POWER_FACTOR, FLOAT32(0x0832)),
+					m(ElectricityMeter.ChannelId.FREQUENCY, FLOAT32(0x0834), SCALE_FACTOR_3)
 			));
 		} else {
 			// 3P3W: no per-phase voltage, power or power-factor; only line-to-line voltage and totals.
 			modbusProtocol.addTask(new FC3ReadRegistersTask(0x0800, Priority.HIGH, //
 					new DummyRegisterElement(0x0800, 0x0805),
-					m(AcrelAdl400Meter.ChannelId.VOLTAGE_L1_L2, float32(0x0806), SCALE_FACTOR_3),
-					m(AcrelAdl400Meter.ChannelId.VOLTAGE_L2_L3, float32(0x0808), SCALE_FACTOR_3),
-					m(AcrelAdl400Meter.ChannelId.VOLTAGE_L3_L1, float32(0x080A), SCALE_FACTOR_3),
-					m(ElectricityMeter.ChannelId.CURRENT_L1, float32(0x080C), SCALE_FACTOR_3),
-					m(ElectricityMeter.ChannelId.CURRENT_L2, float32(0x080E), SCALE_FACTOR_3),
-					m(ElectricityMeter.ChannelId.CURRENT_L3, float32(0x0810), SCALE_FACTOR_3),
+					m(AcrelAdl400Meter.ChannelId.VOLTAGE_L1_L2, FLOAT32(0x0806), SCALE_FACTOR_3),
+					m(AcrelAdl400Meter.ChannelId.VOLTAGE_L2_L3, FLOAT32(0x0808), SCALE_FACTOR_3),
+					m(AcrelAdl400Meter.ChannelId.VOLTAGE_L3_L1, FLOAT32(0x080A), SCALE_FACTOR_3),
+					m(ElectricityMeter.ChannelId.CURRENT_L1, FLOAT32(0x080C), SCALE_FACTOR_3),
+					m(ElectricityMeter.ChannelId.CURRENT_L2, FLOAT32(0x080E), SCALE_FACTOR_3),
+					m(ElectricityMeter.ChannelId.CURRENT_L3, FLOAT32(0x0810), SCALE_FACTOR_3),
 					new DummyRegisterElement(0x0812, 0x0819),
-					m(ElectricityMeter.ChannelId.ACTIVE_POWER, float32(0x081A), SCALE_FACTOR_3_AND_INVERT_IF_TRUE(this.invert)),
+					m(ElectricityMeter.ChannelId.ACTIVE_POWER, FLOAT32(0x081A),
+							SCALE_FACTOR_3_AND_INVERT_IF_TRUE(this.invert)),
 					new DummyRegisterElement(0x081C, 0x0821),
-					m(ElectricityMeter.ChannelId.REACTIVE_POWER, float32(0x0822), SCALE_FACTOR_3_AND_INVERT_IF_TRUE(this.invert)),
+					m(ElectricityMeter.ChannelId.REACTIVE_POWER, FLOAT32(0x0822),
+							SCALE_FACTOR_3_AND_INVERT_IF_TRUE(this.invert)),
 					new DummyRegisterElement(0x0824, 0x0833),
-					m(ElectricityMeter.ChannelId.FREQUENCY, float32(0x0834), SCALE_FACTOR_3)
+					m(ElectricityMeter.ChannelId.FREQUENCY, FLOAT32(0x0834), SCALE_FACTOR_3)
 			));
 		}
-
 		return modbusProtocol;
-	}
-
-	@Override
-	public String debugLog() {
-		return "L:" + this.getActivePower().asString();
-	}
-
-	@Override
-	public MeterType getMeterType() {
-		return this.type;
 	}
 
 	@Override
@@ -215,5 +193,18 @@ public class AcrelAdl400MeterImpl extends AbstractOpenemsModbusComponent
 				ElectricityMeter.getModbusSlaveNatureTable(accessMode), //
 				ModbusSlaveNatureTable.of(AcrelAdl400Meter.class, accessMode, 100).build() //
 		);
+	}
+
+	@Override
+	public String debugLog() {
+		return "L:" + this.getActivePower().asString();
+	}
+
+	private static FloatDoublewordElement FLOAT32(int address) {
+		return new FloatDoublewordElement(address).wordOrder(LSWMSW);
+	}
+
+	private static UnsignedDoublewordElement UINT32(int address) {
+		return new UnsignedDoublewordElement(address).wordOrder(LSWMSW);
 	}
 }
