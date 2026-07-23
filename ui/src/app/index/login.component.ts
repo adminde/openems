b5cdcb3c690452bb3f4181a6d681ef10bf5bfd7a
Adrian Minde <adrian.minde@mail.de>
2026-07-23 09:21:25 +0200

    Use native login inputs to avoid certain password managers not finding the declared fields

--- ui/src/app/index/login.component.ts
+++ ui/src/app/index/login.component.ts
@@ -21,6 +21,7 @@ import { Edge, Service, Utils, Websocket } from "../shared/shared";
 @Component({
     selector: "login",
     templateUrl: "./login.component.html",
+    styleUrls: ["./login.component.scss"],
     standalone: false,
 })
 export class LoginComponent implements ViewWillEnter, AfterContentChecked, OnDestroy {
