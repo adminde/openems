2a27f02123c313f81c8e393b272e55c8e3702964
Adrian Minde <adrian.minde@mail.de>
2026-08-16 10:51:25 +0200

    Fix oros SVG logos and update to 2026.8.0

--- ui/src/themes/oros/environments/theme.ts
+++ ui/src/themes/oros/environments/theme.ts
@@ -1,17 +1,18 @@
 import { Environment, Theme } from "src/environments";
 import { OemMeta } from "./oem-meta";
 
-export const theme: Omit<Environment, "url" | "backend" | "production" | "debugMode"> = {
+export const theme: Omit<
+    Environment,
+    "url" | "backend" | "production" | "debugMode"
+> = {
     theme: "OpenEMS" as Theme,
 
     uiTitle: "OROS ENERGY",
     uiTitleShort: "OROS",
-    edgeShortName: "OR/OS",
-    edgeLongName: "OR/OS Energy Management System",
-    headerLogo: "oros-logo-black-text.svg",
-    headerTitleShow: false,
     footerTextHtml: "OR/OS",
-    defaultLanguage: "de",
+    edgeShortName: "OR/OS",
+    edgeLongName: "OROS Energy Management System",
+    forceNewNavigation: true,
 
     docsUrlPrefix: "https://github.com/OpenEMS/openems/blob/develop/",
     PRODUCT_TYPES: () => null,
